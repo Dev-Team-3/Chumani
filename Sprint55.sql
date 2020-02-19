af3eb165762f96a6195317301d4a7a6c801714d7
Chumani17 <61044452+Chumani17@users.noreply.github.com>
2020-02-19 09:32:00 +0200

    Add files via upload

---
+++ Sprint55.sql
@@ -0,0 +1,7 @@
+Chips	Quantity	Price	Cooldrinks	Quantity	Price	Pies	Quantity	Price	Fruit	Quantity	Price	Cupcakes	Quantity	price	Veggies	Quantity	Price
+Simba	30	R20	Coke	50	R10	Pepper Steak	40	R20	Pear	35	R2	Vanilla	65	R7	Spinach	40	R15
+Lays	45	R21	Fanta	50	R10	Chicken	50	R22	Apple	45	R2	Chocolate	40	R10	cabbage	80	R25
+Fritoz	25	R14	Sprite	50	R10	Steak & kidney	45	R25	Orange	40	R3	Red velevet	55	R15	carrot	50	R30
+Niks naks	45	R15	Pepsi	50	R10	Vegan	30	R23	peach	35	R5	Caramel	35	R8	butternut	70	R45
+Pretzels	40	R18	Stoney	50	R10	sausage	25	R24	grape	50	R20	Bran	40	R9	squash	50	R20
+Squirls	25	R12	Aqueelah	50	R12	burger pie	30	R25	Pawpaw	60	R18	Blueberry	60	R6	lentils	35	R25
